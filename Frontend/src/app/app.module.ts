69321362dd3bf39ae92732bb2a66b605d33d173e
Tiago Silva <tiagobsb84@gmail.com>
2023-02-02 20:20:39 -0300

    Header e footer finalizados

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -7,11 +7,13 @@ import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
